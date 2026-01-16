7f93a799dd485c7fc4d528b14d602ef72625432f
Nishant Sharma <nishantsharma2003indian.com>
2026-01-16 19:17:09 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -9,9 +12,26 @@ int main() {
   // TODO: Uncomment the code below to pass the first stage
   while(true){
     std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    std::stringstream ss(input);
+
     std::string command;
-    std::cin >> command;
+    ss>>command;
+    
     if (command == "exit") break;
-    std:: cout << command <<": command not found\n";
+    else if(command == "echo") {
+      // std:: string args = ss.str();
+      // std::cout << args << "\n";
+      while(ss){
+        std::string args;
+        ss >> args;
+        std::cout << args << " ";
+      }
+      std::cout << "\n";
+    }else {
+      std:: cout << command <<": command not found\n";
+    }
+
   }
 }
